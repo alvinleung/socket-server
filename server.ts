bc7b4f077aba5f3fdca7e949fa96e9e7b2489c3c
alvin leung <alvinleung2009@gmail.com>
2025-05-17 23:31:29 -0700

    show server ip

--- server.ts
+++ server.ts
@@ -1,4 +1,5 @@
 import { createServer } from "http";
+import { networkInterfaces } from "os";
 import { Server, Socket } from "socket.io";
 
 const httpServer = createServer();
@@ -22,6 +23,22 @@ io.on("connection", (socket: Socket) => {
 });
 
 const PORT = 3000;
+
+// Get the system IP address
+const getSystemIP = () => {
+  const nets = networkInterfaces();
+  for (const name of Object.keys(nets)) {
+    for (const net of nets[name] || []) {
+      // Skip over non-IPv4 and internal (i.e. 127.0.0.1) addresses
+      if (net.family === "IPv4" && !net.internal) {
+        return net.address;
+      }
+    }
+  }
+  return "localhost"; // Fallback to localhost if no external IP is found
+};
+
+const IP = getSystemIP();
 httpServer.listen(PORT, () => {
-  console.log(`WebSocket server running on http://localhost:${PORT}`);
+  console.log(`WebSocket server running on http://${IP}:${PORT}`);
 });
